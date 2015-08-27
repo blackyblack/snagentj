5326bede5be998ea3d89624683c890fd09432a43
Andrew Lekar <johnmalkovich@mail.ru>
2015-08-27 14:14:32 +0500

    Moved to SuperNET.agents

--- src/snagentj/Framework.java
+++ src/snagentj/Framework.java
@@ -104,7 +104,7 @@ public class Framework
 
       System.out.println("Parent PID is " + info.ppid);
 
-      info.connectaddr = "ipc://SuperNET";
+      info.connectaddr = "ipc://SuperNET.agents";
       info.bindaddr = "ipc://" + info.daemonid;
       /*if(info.permanentflag)
       {
